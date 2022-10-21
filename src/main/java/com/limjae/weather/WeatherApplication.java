89a591c9e98b1cf362e8a206abf9060d4614d1f6
limjae <limjae96@gmail.com>
2022-10-21 21:38:11 +0900

    [Feat] TimeZone Setting

--- src/main/java/com/limjae/weather/WeatherApplication.java
+++ src/main/java/com/limjae/weather/WeatherApplication.java
@@ -5,10 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableBatchProcessing
 @EnableScheduling
 public class WeatherApplication {
+	@PostConstruct
+	public void setTimeZone(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
