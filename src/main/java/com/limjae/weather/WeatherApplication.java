1e15676957f8ba0e6f5561f866951f26aeb8e445
limjae <limjae96@gmail.com>
2022-10-21 21:48:53 +0900

    [Fix] TimeZone Setting

--- src/main/java/com/limjae/weather/WeatherApplication.java
+++ src/main/java/com/limjae/weather/WeatherApplication.java
@@ -1,20 +1,26 @@
 package com.limjae.weather;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.Locale;
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableBatchProcessing
 @EnableScheduling
+@Slf4j
 public class WeatherApplication {
 	@PostConstruct
 	public void setTimeZone(){
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		Locale.setDefault(Locale.KOREA);
+		log.info("Server Time : {}", LocalDateTime.now());
 	}
 
 	public static void main(String[] args) {
